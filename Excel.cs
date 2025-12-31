c25de5017930a65d1623dffc3c45e8cede9244e2
Manuel Zarat <manuel.zarat@gmail.com>
2025-12-31 21:52:23 +0100

    Update Excel.cs

--- Excel.cs
+++ Excel.cs
@@ -17,33 +17,35 @@ namespace Excel
 
             var routines = new List<Routine>();
 
-            routines.Add(new Routine((Type)null, "xlsx_new"));
-            routines.Add(new Routine((Type)null, "xlsx_load", (Type)null, "Loads an xlsx file from the specified path."));
-            routines.Add(new Routine((Type)null, "xlsx_close", (Type)null, "close an open workbook."));
+            routines.Add(new Routine((Type)null, "xlsx.new"));
+            routines.Add(new Routine((Type)null, "xlsx.load", (Type)null, "Loads an xlsx file from the specified path. params: path"));
+            routines.Add(new Routine((Type)null, "xlsx.close", (Type)null, "close an open workbook. params: workbook handle"));
 
-            routines.Add(new Routine((Type)null, "xlsx_add_ws", (Type)null, (Type)null, "add a worksheet to a workbook."));
-            routines.Add(new Routine((Type)null, "xlsx_get_ws", (Type)null, (Type)null, "get a worksheet from a workbook."));
-            routines.Add(new Routine((Type)null, "xlsx_remove_ws", (Type)null, (Type)null, "remove a worksheet from a workbook."));
-            routines.Add(new Routine((Type)null, "xlsx_list_ws", (Type)null, "list all the worksheets from a workbook."));
+            routines.Add(new Routine((Type)null, "xlsx.add_ws", (Type)null, (Type)null, "add a worksheet to a workbook. params: workbook handle, sheet name"));
+            routines.Add(new Routine((Type)null, "xlsx.get_ws", (Type)null, (Type)null, "get a worksheet from a workbook. params workbook handle, sheet name"));
+            routines.Add(new Routine((Type)null, "xlsx.remove_ws", (Type)null, (Type)null, "remove a worksheet from a workbook. params: workbook handle, sheet name"));
+            routines.Add(new Routine((Type)null, "xlsx.list_ws", (Type)null, "list all the worksheets from a workbook as array. params: workbook handle"));
 
-            routines.Add(new Routine((Type)null, "xlsx_rows", (Type)null));
-            routines.Add(new Routine((Type)null, "xlsx_cols", (Type)null));
+            routines.Add(new Routine((Type)null, "xlsx.rows", (Type)null, "Total number of rows in this sheet. params: sheet"));
+            routines.Add(new Routine((Type)null, "xlsx.usedrows", (Type)null, "Number of used rows in this sheet. params: sheet"));
+            routines.Add(new Routine((Type)null, "xlsx.columns", (Type)null, "Total number of columns in this sheet. params: sheet"));
+            routines.Add(new Routine((Type)null, "xlsx.usedcolumns", (Type)null, "Number of used columns in this sheet. params: sheet"));
 
             List<Type> cellParams = new List<Type>();
             cellParams.Add((Type)null);
             cellParams.Add(typeof(int));
             cellParams.Add(typeof(int));
             cellParams.Add((Type)null);
-            routines.Add(new Routine((Type)null, "xlsx_set", cellParams));
-            routines.Add(new Routine((Type)null, "xlsx_set_formula", cellParams));
-            routines.Add(new Routine((Type)null, "xlsx_get", (Type)null, typeof(int), typeof(int)));
-            routines.Add(new Routine((Type)null, "xlsx_get_formula", (Type)null, typeof(int), typeof(int)));
+            routines.Add(new Routine((Type)null, "xlsx.set", cellParams, "Set the value of a cell. params: sheet, row, col, value"));
+            routines.Add(new Routine((Type)null, "xlsx.set_formula", cellParams, "Set the value of a cell to be a formula. params: sheet, row, col, formula"));
+            routines.Add(new Routine((Type)null, "xlsx.get", (Type)null, typeof(int), typeof(int), "Get the value of a cell. params: sheet, row, col"));
+            routines.Add(new Routine((Type)null, "xlsx.get_formula", (Type)null, typeof(int), typeof(int), "Get the formula of a cell. params: sheet, row, col"));
 
             List<Type> csvParams = new List<Type>();
             csvParams.Add((Type)null);        // sheet (IXLWorksheet)
             csvParams.Add(typeof(string));    // separator, z.B. ";", ",", "\t"
             csvParams.Add(typeof(bool));      // quoteFields (always quote)
-            routines.Add(new Routine((Type)null, "xlsx_tocsv", csvParams, "params: sheet, separator, quoteFields"));
+            routines.Add(new Routine((Type)null, "xlsx.tocsv", csvParams, "Get a sheet as csv string. params: sheet, separator, quoteFields"));
 
             exportedRoutines = routines.AsReadOnly();
 
@@ -53,30 +55,30 @@ namespace Excel
 
         public object Invoke(string routine, List<object> parameters) {
 
-            if(routine == "xlsx_new")
+            if(routine == "xlsx.new")
             {
                 return new XLWorkbook();
             }
-            if(routine == "xlsx_load")
+            if(routine == "xlsx.load")
             {
                 string path = (string)parameters[0];
                 XLWorkbook wb = new XLWorkbook(path);
                 return wb;
             }
-            if(routine == "xlsx_close")
+            if(routine == "xlsx.close")
             {
                 XLWorkbook wb = parameters[0] as XLWorkbook;
                 wb.Dispose();
                 return null;
             }
 
-            if (routine == "xlsx_add_ws")
+            if (routine == "xlsx.add_ws")
             {
                 XLWorkbook wb = parameters[0] as XLWorkbook;            
                 var sheet = wb.Worksheets.Add((string)parameters[1]);
                 return sheet;
             }
-            if (routine == "xlsx_get_ws")
+            if (routine == "xlsx.get_ws")
             {
                 XLWorkbook wb = (XLWorkbook)parameters[0];
                 try
@@ -89,32 +91,33 @@ namespace Excel
                     return null;
                 }
             }
-            if(routine == "xlsx_remove_ws")
+            if(routine == "xlsx.remove_ws")
             {
                 XLWorkbook wb = parameters[0] as XLWorkbook;
                 wb.Worksheets.Delete((string)parameters[1]);
                 return null;
             }
-            if(routine == "xlsx_list_ws")
+            if(routine == "xlsx.list_ws")
             {
                 XLWorkbook wb = parameters[0] as XLWorkbook;
-                List<string> sheetNames = new List<string>();
+                //List<string> sheetNames = new List<string>();
+                ArrayList ret = new ArrayList();
                 foreach(var sheet in wb.Worksheets)
                 {
-                    sheetNames.Add(sheet.Name);
+                    ret.Add(sheet.Name);
                 }
-                return sheetNames;
+                return ret;
             }
 
 
-            if (routine == "xlsx_get")
+            if (routine == "xlsx.get")
             {
                 var sheet = parameters[0] as IXLWorksheet;
                 int row = (int)parameters[1];
                 int col = (int)parameters[2];
                 return sheet.Cell(row, col).Value;
             }
-            if (routine == "xlsx_set")
+            if (routine == "xlsx.set")
             {
                 var sheet = parameters[0] as IXLWorksheet;
                 int row = (int)parameters[1];
@@ -159,7 +162,7 @@ namespace Excel
 
                 return null;
             }
-            if(routine == "xlsx_set_formula")
+            if(routine == "xlsx.set_formula")
             {
                 var sheet = parameters[0] as IXLWorksheet;
                 int row = (int)parameters[1];
@@ -168,25 +171,45 @@ namespace Excel
                 sheet.Cell(row, col).FormulaA1 = formula;
                 return null;
             }
-            if(routine == "xlsx_get_formula")
+            if(routine == "xlsx.get_formula")
             {
                 var sheet = parameters[0] as IXLWorksheet;
                 int row = (int)parameters[1];
                 int col = (int)parameters[2];
                 return sheet.Cell(row, col).FormulaA1;
             }
-            if (routine == "xlsx_rows")
+
+            if (routine == "xlsx.rows")
+            {
+                var sheet = parameters[0] as IXLWorksheet;
+                if (sheet == null) return 0;
+
+                // Letzte Zeile mit echtem Inhalt (Wert/Formel), nicht nur Format/Validation
+                var lastRow = sheet.LastRowUsed(XLCellsUsedOptions.Contents)?.RowNumber() ?? 0;
+                return lastRow;
+            }
+
+            if (routine == "xlsx.columns")
+            {
+                var sheet = parameters[0] as IXLWorksheet;
+                if (sheet == null) return 0;
+
+                var lastCol = sheet.LastColumnUsed(XLCellsUsedOptions.Contents)?.ColumnNumber() ?? 0;
+                return lastCol;
+            }
+
+            if (routine == "xlsx.usedrows")
             {
                 var sheet = parameters[0] as IXLWorksheet;
                 return sheet.RowsUsed().Count();
             }
-            if(routine == "xlsx_cols")
+            if (routine == "xlsx.usedcolumns")
             {
                 var sheet = parameters[0] as IXLWorksheet;
                 return sheet.ColumnsUsed().Count();
             }
 
-            if (routine == "xlsx_tocsv")
+            if (routine == "xlsx.tocsv")
             {
                 var sheet = parameters[0] as IXLWorksheet;
                 if (sheet == null) return "";
